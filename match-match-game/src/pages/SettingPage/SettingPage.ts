8fe177fea21eb3284f4b8de5827ff4eec60ddcaa
Sergei Demchuk <vinci.ru@mail.ru>
2021-05-30 21:27:35 +0300

    fix: fix setting page

--- match-match-game/src/pages/SettingPage/SettingPage.ts
+++ match-match-game/src/pages/SettingPage/SettingPage.ts
@@ -28,29 +28,18 @@ export class SettingPage extends ClearFields {
       <div class="setting-field__title">Game Settings:</div>
 
       <div class="settings">
-          <div class="setting_item">
-              <div class="cards-type-menu__title">Game cards:</div>
-              <div class="form-type-game">
-                  <select id="type" name="select_type">
-                      <option selected="selected">Select game cards type</option>
-                      <option id="animal">Nature</option>
-                      <option id="cars">Supernatural</option>
-                      <option id="cars">Wild animals</option>
-                  </select>
-              </div>
-          </div>
+        <div class="setting_item">
+            <div class="cards-type-menu__title">Game cards:</div>
+            <div id="Nature" class="cardsType set_typecard">Nature</div>
+            <div id="Supernatural" class="cardsType set_typecard">Supernatural</div>
+            <div id="Wild animals" class="cardsType set_typecard">Wild animals</div>
+        </div>
 
-          <div class="setting_item">
-              <div class="cards-type-menu__title">Difficulty:</div>
-              <form class="form-type-game">
-                  <select id="difficult" name="select_difficulty">
-                      <option selected="selected" style="font-family: 'Adigiana2'">Select game difficulty</option>
-                      <option id="fourth">4x4</option>
-                      <option id="six">6x6</option>
-                  </select>
-              </form>
-
-          </div>
+        <div class="setting_item">
+            <div class="cards-type-menu__title">Difficulty:</div>
+            <div id="4x4" class="cardsType set_difficulty">4x4</div>
+            <div id="6x6" class="cardsType set_difficulty">6x6</div>
+        </div>
       </div>
       `;
     if (!this.body) throw Error('App element not found');
@@ -59,20 +48,43 @@ export class SettingPage extends ClearFields {
     // this.headerBtn.classList.add('active');
   }
 
-  setDifficulty = () => {
-    const type = document.querySelector('#type');
-    if (!type) throw Error('App element not found');
-
-    type.addEventListener('change', (e: any) => {
-      localStorage.setItem('category', e.target.value);
-      console.log(e.target.value);
+  setDifficulty() {
+    (document.getElementById('Nature') as HTMLElement).addEventListener('click', () => {
+        localStorage.setItem('category', 'Nature');
+        this.clearColorBtns('set_typecard', 'Nature');
     });
 
-    const difficult = document.getElementById('difficult');
-    if (!difficult) throw Error('App element not found');
+    (document.getElementById('Supernatural') as HTMLElement).addEventListener('click', () => {
+        localStorage.setItem('category', 'Supernatural');
+        this.clearColorBtns('set_typecard', 'Supernatural');
+    });
 
-    difficult.addEventListener('change', (e: any) => {
-      localStorage.setItem('difficulty', e.target.value);
+    (document.getElementById('Wild animals') as HTMLElement).addEventListener('click', () => {
+        localStorage.setItem('category', 'Wild animals');
+        this.clearColorBtns('set_typecard', 'Wild animals');
+    });
+
+    (document.getElementById('4x4') as HTMLElement).addEventListener('click', () => {
+        localStorage.setItem('difficulty', '4x4');
+        this.clearColorBtns('set_difficulty', '4x4');
+    });
+
+    (document.getElementById('6x6') as HTMLElement).addEventListener('click', () => {
+        localStorage.setItem('difficulty', '6x6');
+        this.clearColorBtns('set_difficulty', '6x6');
+    });
+
+    const nowTypeGame = localStorage.getItem('category');
+    const nowDiffcaltyGame = localStorage.getItem('difficulty');
+
+    (document.getElementById(nowTypeGame || 'Nature') as HTMLElement).classList.add('active');
+    (document.getElementById(nowDiffcaltyGame || '4x4') as HTMLElement).classList.add('active');
+  }
+
+  clearColorBtns = (typeSetting:string, selectBtn:string) => {
+    document.querySelectorAll(`.${typeSetting}`).forEach((btn) => {
+        btn.classList.remove('active');
+        (document.getElementById(`${selectBtn}`) as HTMLElement).classList.add('active');
     });
   };
 }
